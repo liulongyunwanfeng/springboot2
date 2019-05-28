7412ef41c9d8f37fe8aecfdaf688c3759719624d
liulongyun <liulongyunwanfeng@163.com>
2019-05-28 18:52:39 +0800

    ss配置l

--- src/main/java/com/django/springboot2/confirguetion/SSLConfiguration.java
+++ src/main/java/com/django/springboot2/confirguetion/SSLConfiguration.java
@@ -16,7 +16,7 @@ import org.springframework.context.annotation.Configuration;
  * @Version 1.0
  * @Description TODO
  */
-@Configuration
+
 public class SSLConfiguration {
 
     /**
@@ -49,9 +49,9 @@ public class SSLConfiguration {
     public Connector httpConnector(){
         Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
         connector.setScheme("http");
-        connector.setPort(80);
+        connector.setPort(8080);
         connector.setSecure(false);
-        connector.setRedirectPort(8005);
+        connector.setRedirectPort(8443);
         return  connector;
     }
 }
